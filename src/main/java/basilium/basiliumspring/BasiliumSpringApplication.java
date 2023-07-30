ad422c4472745786fb966c99d3d5df3c082b2027
박한솔 <hansol@baghansol-ui-MacBookPro.local>
2023-07-31 00:19:32 +0900

    init : server add[박한솔]

---
+++ src/main/java/basilium/basiliumspring/BasiliumSpringApplication.java
@@ -0,0 +1,13 @@
+package basilium.basiliumspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasiliumSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasiliumSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/basilium/basiliumspring/BasiliumSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package basilium.basiliumspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasiliumSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
