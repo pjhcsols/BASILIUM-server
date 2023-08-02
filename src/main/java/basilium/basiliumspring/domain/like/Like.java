517680f340a401239360b6841eb26027ec0b332b
kimmokalover <kimmokalover@gmail.com>
2023-08-02 17:49:03 +0900

    feat:like 도메인 생성 [김승준]

---
+++ src/main/java/basilium/basiliumspring/domain/like/Like.java
@@ -0,0 +1,31 @@
+package basilium.basiliumspring.domain.like;
+
+public class Like {
+    private Long id;
+    private String userId;
+    private Long productId;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public Long getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Long productId) {
+        this.productId = productId;
+    }
+}
