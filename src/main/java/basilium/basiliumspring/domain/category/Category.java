374f4ec7d946f4d60168a7cbc3757d14ea0bc9d3
pjhcsols <pjhcsols@naver.com>
2023-08-02 18:25:11 +0900

    feat: Category 도메인 생성 [박한솔]

---
+++ src/main/java/basilium/basiliumspring/domain/category/Category.java
@@ -0,0 +1,29 @@
+package basilium.basiliumspring.domain.category;
+
+public class Category {
+    private Long categoryId;
+    private String categoryName;
+
+    public Category(Long categoryId, String categoryName) {
+        this.categoryId = categoryId;
+        this.categoryName = categoryName;
+    }
+
+
+    public Long getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(Long categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+}
