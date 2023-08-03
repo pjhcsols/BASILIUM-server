6533a76583dd88eb40b6630db3a8822a4af2ca22
pjhcsols <pjhcsols@naver.com>
2023-08-03 14:38:19 +0900

    feat: Review 도메인 생성 [박한솔]

---
+++ src/main/java/basilium/basiliumspring/domain/review/Review.java
@@ -0,0 +1,59 @@
+package basilium.basiliumspring.domain.review;
+
+public class Review {
+    private Long reviewId;
+    private Long userId;
+    private Long productId;
+    private String review;
+    private String reviewDate;
+
+
+    public Review(Long reviewId, Long userId, Long productId, String review, String reviewDate) {
+        this.reviewId = reviewId;
+        this.userId = userId;
+        this.productId = productId;
+        this.review = review;
+        this.reviewDate = reviewDate;
+    }
+
+
+    public Long getReviewId() {
+        return reviewId;
+    }
+
+    public void setReviewId(Long reviewId) {
+        this.reviewId = reviewId;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public Long getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Long productId) {
+        this.productId = productId;
+    }
+
+    public String getReview() {
+        return review;
+    }
+
+    public void setReview(String review) {
+        this.review = review;
+    }
+
+    public String getReviewDate() {
+        return reviewDate;
+    }
+
+    public void setReviewDate(String reviewDate) {
+        this.reviewDate = reviewDate;
+    }
+}
