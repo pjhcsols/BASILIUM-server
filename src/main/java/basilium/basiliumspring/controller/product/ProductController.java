5f6cf071c257ea8325ad39697c9c1166299d6039
pjhcsols <pjhcsols@naver.com>
2023-08-14 20:20:22 +0900

    feat: Product service&controller 생성[박한솔]

--- src/main/java/basilium/basiliumspring/controller/product/ProductController.java
+++ src/main/java/basilium/basiliumspring/controller/product/ProductController.java
@@ -1,4 +1,61 @@
 package basilium.basiliumspring.controller.product;
 
+import basilium.basiliumspring.domain.product.Product;
+import basilium.basiliumspring.service.product.ProductService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/products")
 public class ProductController {
+
+    private ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @PostMapping
+    public ResponseEntity<String> addProduct(@RequestBody Product product) {
+        productService.addProduct(product);
+        return ResponseEntity.status(HttpStatus.CREATED).body("Product added successfully");
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Product>> getAllProducts() {
+        List<Product> products = productService.getAllProducts();
+        return ResponseEntity.ok(products);
+    }
+
+    @GetMapping("/{productId}")
+    public ResponseEntity<Product> getProductById(@PathVariable Long productId) {
+        Product product = productService.getProductById(productId);
+        if (product != null) {
+            return ResponseEntity.ok(product);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PutMapping("/{productId}")
+    public ResponseEntity<String> updateProduct(@PathVariable Long productId, @RequestBody Product updatedProduct) {
+        updatedProduct.setProductId(productId);
+        productService.updateProduct(updatedProduct);
+        return ResponseEntity.ok("Product updated successfully");
+    }
+
+    @DeleteMapping("/{productId}")
+    public ResponseEntity<String> deleteProductById(@PathVariable Long productId) {
+        productService.deleteProductById(productId);
+        return ResponseEntity.ok("Product deleted successfully");
+    }
+
+    @DeleteMapping("/name/{productName}")
+    public ResponseEntity<String> deleteProductByName(@PathVariable String productName) {
+        productService.deleteProductByName(productName);
+        return ResponseEntity.ok("Products with name " + productName + " deleted successfully");
+    }
 }
--- src/main/java/basilium/basiliumspring/service/product/ProductService.java
+++ src/main/java/basilium/basiliumspring/service/product/ProductService.java
@@ -1,4 +1,39 @@
 package basilium.basiliumspring.service.product;
 
+import basilium.basiliumspring.domain.product.Product;
+import basilium.basiliumspring.repository.product.ProductRepository;
+
+import java.util.List;
+
 public class ProductService {
+
+    private ProductRepository productRepository;
+
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public void addProduct(Product product) {
+        productRepository.addProduct(product);
+    }
+
+    public List<Product> getAllProducts() {
+        return productRepository.getAllProducts();
+    }
+
+    public Product getProductById(Long productId) {
+        return productRepository.getProductById(productId);
+    }
+
+    public void updateProduct(Product updatedProduct) {
+        productRepository.updateProduct(updatedProduct);
+    }
+
+    public void deleteProductById(Long productId) {
+        productRepository.deleteProductById(productId);
+    }
+
+    public void deleteProductByName(String productName) {
+        productRepository.deleteProductByName(productName);
+    }
 }
